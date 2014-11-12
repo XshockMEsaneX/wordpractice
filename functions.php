0934af0b2d2ca93e5377e2207b9624236dd3062a
Matt Cebulak <Matt@matthews-mbp.attlocal.net>
2014-11-11 20:11:33 -0600

    Setup theme.js

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function load_scripts() {
 	wp_enqueue_style( 'bootstrap', get_template_directory_uri(). '/assets/vendor/bootstrap/css/bootstrap.min.css', false );
 	wp_enqueue_script( 'bootstrap', get_template_directory_uri(). '/assets/vendor/bootstrap/js/bootstrap.min.js', array( 'jquery' ) );
 	wp_enqueue_style( 'theme', get_template_directory_uri(). '/assets/css/theme.css', 'bootstrap' );
+	wp_enqueue_script( 'theme', get_template_directory_uri(). '/assets/js/theme.js', array( 'jquery', 'bootstrap' ) );
 }
 
 add_action( 'wp_enqueue_scripts', 'load_scripts' );
